8b07a8b2ec1570c4e86cd42b55aed74a418802e4
madde <madde@dd647c6c5f9a>
2019-06-18 09:32:47 +0000

    coraData instead of bookkeeper data

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,5 +1,6 @@
 module se.uu.ub.cora.searchstorage {
 	requires transitive se.uu.ub.cora.bookkeeper;
+	requires transitive se.uu.ub.cora.data;
 
 	exports se.uu.ub.cora.searchstorage;
 }
--- src/main/java/se/uu/ub/cora/searchstorage/SearchStorage.java
+++ src/main/java/se/uu/ub/cora/searchstorage/SearchStorage.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2017 Uppsala University Library
+ * Copyright 2017, 2019 Uppsala University Library
  *
  * This file is part of Cora.
  *
@@ -19,7 +19,7 @@
 
 package se.uu.ub.cora.searchstorage;
 
-import se.uu.ub.cora.bookkeeper.data.DataGroup;
+import se.uu.ub.cora.data.DataGroup;
 
 public interface SearchStorage {
 
